ad393c8883835edd6178dcde89746cafb8468d9f
Ramon Costa <rrcostapatel@gmail.com>
2022-04-18 18:43:15 -0700

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,34 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greeting = "Yo! Wassup!";
+let name = 'Memon';
+let title = "Leader of Wolves";
+let place = "Dead forest";
+
+// TODO: Create 3-Integer variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 150;
+let year = 1295;
+let weaponsNeeded = 7;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weapons = ['Sword','Dagger','Spear','Staff','Wand'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasWeaponsNeeded = false;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greeting+' You can call me '+name);
+console.log('You can also call me by my title, ' + title + '.');
+console.log("I'm not a normal person. For example, I just turned " + age + " years old.");
+console.log("You should be able to figure out when I was born on your own, since you already know this year is "+year+'.');
+console.log("I spend most of my time in the " + place + '.');
+console.log("Visitor: Do you have any weapons?");
+console.log(name+": Yes, I got 5.");
+console.log("Visitor: Which one's your favorite?");
+console.log(name+": Yes, my favourite weapon is "+weapons[1]);
+console.log("Visitor: Do you have all the weapons you need?");
+console.log(name+": No, "+hasWeaponsNeeded+". I actually need "+weaponsNeeded+" weapons.");
